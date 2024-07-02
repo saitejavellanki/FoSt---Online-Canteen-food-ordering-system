3c01d65967c0f95e2cfea15b45fa7069832b03cd
saitejavellanki <95115567+saitejavellanki@users.noreply.github.com>
2024-07-02 15:04:07 +0530

    installed react-icons added header added pages added some routings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
+import { BrowserRouter, Route, Routes} from "react-router-dom"
+import styles from "./App.scss";
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element ={<Home/>} />
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,57 @@
 import React from 'react'
 import styles from "./Header.module.scss"
+import {FaShoppingCart} from "react-icons/fa"
+import { Link } from 'react-router-dom'
+
+
+const logo = (
+    <div className={styles.logo}>
+    <Link to="/">
+      <h2>
+        Fo<span>St</span>.
+      </h2>
+    </Link>
+              
+    </div>
+    )
+
+
+const cart = (
+        <span className={styles.cart}>
+                  <Link to="/cart">
+                    Cart
+                    <FaShoppingCart size={20}/>
+                    <p>0</p>
+                  </Link>
+                  </span>
+      )
+                  
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+        <div className={styles.header}>
+        {logo}
+        <nav>
+            <ul>
+               <li>
+                <Link to="/">
+                Home
+                </Link>
+               </li>
+            </ul>
+            <div className={styles["header-right"]}>
+            <span className={styles.links}>
+                <Link>Login</Link>
+                <Link>Register</Link>
+                <Link>Order-history</Link>
+
+            </span>
+            {cart}
+            </div>
+        </nav>
+        </div>
+    </header>
   )
 }
 
