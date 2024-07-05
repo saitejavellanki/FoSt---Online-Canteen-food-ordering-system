51a329820f8c56e9fa2a844c188fe18e8abf7d13
Ambati Niranjana Srinivasu <ambati.ns04@gmail.com>
2024-07-06 01:41:22 +0530

    Changed Loader image

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -11,7 +11,7 @@ const Register = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [cPassword, setCPassword] = useState("")
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
 
     const navigate = useNavigate()
 
